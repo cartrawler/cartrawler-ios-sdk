8f038d9506b7db9790315bc84dade6583f723504
Rafael Ortiz <rortiz@cartrawler.com>
2019-02-15 18:12:35 +0000

    Release 8.0.0

--- CarTrawlerSDK.framework/Headers/CTStyle.h
+++ CarTrawlerSDK.framework/Headers/CTStyle.h
@@ -8,21 +8,41 @@
 
 #import <UIKit/UIKit.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface CTStyle : NSObject
 
-@property (nonatomic, readonly) UIColor *primaryColor;
-@property (nonatomic, readonly) UIColor *secondaryColor;
-@property (nonatomic, readonly) UIColor *accentColor;
+@property (nonatomic, strong, readonly) UIColor *primaryColor;
+@property (nonatomic, strong, readonly) UIColor *primaryDarkColor;
+@property (nonatomic, strong, readonly) UIColor *primaryLightColor;
 
-@property (nonatomic, readonly) UIFont *regularFont;
-@property (nonatomic, readonly) UIFont *boldFont;
-@property (nonatomic, readonly) UIFont *italicFont;
+@property (nonatomic, strong, readonly) UIColor *ctaColor;
+@property (nonatomic, strong, readonly) UIColor *ctaFontColor;
+
+@property (nonatomic, strong, readonly) UIColor *linkColor;
+@property (nonatomic, strong, readonly) UIColor *backgroundColor;
+
+/* Deprecated */
+@property (nonatomic, strong, readonly) UIColor *secondaryColor DEPRECATED_ATTRIBUTE;
+@property (nonatomic, strong, readonly) UIColor *accentColor DEPRECATED_ATTRIBUTE;
+
+@property (nonatomic) UIFont *regularFont DEPRECATED_ATTRIBUTE;
+@property (nonatomic) UIFont *boldFont DEPRECATED_ATTRIBUTE;
+@property (nonatomic) UIFont *italicFont DEPRECATED_ATTRIBUTE;
 
 - (instancetype)initWithPrimaryColor:(UIColor *)primaryColor
                       secondaryColor:(UIColor *)secondaryColor
                          accentColor:(UIColor *)accentColor
-                         regularFont:(UIFont *)regularFont
-                            boldFont:(UIFont *)boldFont
-                          italicFont:(UIFont *)italicFont;
+                         regularFont:(nullable UIFont *)regularFont
+                            boldFont:(nullable UIFont *)boldFont
+                          italicFont:(nullable UIFont *)italicFont;
+
++ (instancetype)styleWithPrimaryColor:(UIColor *)primaryColor
+                    primaryLightColor:(nullable UIColor *)primaryLightColor
+                     primaryDarkColor:(nullable UIColor *)primaryDarkColor
+                             ctaColor:(nullable UIColor *)color
+                         ctaFontColor:(nullable UIColor *)ctaFontColor;
 
 @end
+
+NS_ASSUME_NONNULL_END
--- CarTrawlerSDK.podspec
+++ CarTrawlerSDK.podspec
@@ -2,7 +2,7 @@
 Pod::Spec.new do |s|
 
 s.name         = "CarTrawlerSDK"
-s.version      = "7.2.2"
+s.version      = "8.0.0"
 s.summary      = "Cartrawler iOS SDK"
 s.description  = <<-DESC
 A toolkit for car rental & ground transport
