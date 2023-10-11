edeade4bd4fb1c71b0f9f2a0055e7a89cbbd0928
ejunior <ejunior@cartrawler.com>
2023-10-11 10:35:06 +0100

    Release 14.8.0

---
+++ CarTrawlerSDK.xcframework/ios-arm64_x86_64-simulator/CarTrawlerSDK.framework/Headers/CTFlightPassengerBreakdown.h
@@ -0,0 +1,26 @@
+//
+//  CTFlightPassengerBreakdown.h
+//  CarTrawlerSDK
+//
+//  Created by Rafael Ortiz on 02/10/2023.
+//  Copyright © 2023 Cartrawler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTFlightPassengerBreakdown : NSObject
+
+@property (nonatomic) NSUInteger adults;
+@property (nonatomic) NSUInteger teens;
+@property (nonatomic) NSUInteger children;
+@property (nonatomic) NSUInteger infants;
+
+- (nonnull instancetype)initWithAdults:(NSUInteger)adults teens:(NSUInteger)teens children:(NSUInteger)children infants:(NSUInteger)infants;
+- (NSDictionary *)toDictionary;
+- (NSNumber *)totalPax;
+
+@end
+
+NS_ASSUME_NONNULL_END
