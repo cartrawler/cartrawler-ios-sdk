a8a050417d0a86e899972b376acbbc20cd0e6074
rsortiz <sh3llbr2@gmail.com>
2024-09-16 11:45:22 +0100

    Release 15.6.0

---
+++ CarTrawlerSDK.xcframework/ios-arm64_x86_64-maccatalyst/CarTrawlerSDK.framework/Versions/A/Headers/CTVehicleSearch.h
@@ -0,0 +1,38 @@
+//
+//  CTVehicleSearch.h
+//  CarTrawlerSDK
+//
+//  Created by Rafael Ortiz on 03/09/2024.
+//  Copyright © 2024 Cartrawler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTVehicleSearch : NSObject <NSCoding, NSCopying, NSSecureCoding>
+
+@property (nonatomic, readonly) BOOL enableLocalNotification;
+@property (nonatomic, strong, readonly) NSString *implementationID;
+@property (nonatomic, strong, readonly) NSString *clientID;
+@property (nonatomic, strong, readonly) NSString *pickupIATA;
+@property (nonatomic, strong, readonly) NSString *dropoffIATA;
+
+@property (nonatomic, strong, readonly) NSString *pickupLocationID;
+@property (nonatomic, strong, readonly) NSString *dropoffLocationID;
+
+@property (nonatomic, strong, readonly) NSString *age;
+@property (nonatomic, strong, readonly) NSString *language;
+@property (nonatomic, strong, readonly) NSString *countryCode;
+@property (nonatomic, strong, readonly) NSString *currencyCode;
+
+@property (nonatomic, strong, readonly) NSString *pinnedVehicleID;
+
+@property (nonatomic, strong, readonly) NSDate *derivedPickupDate;
+@property (nonatomic, strong, readonly) NSDate *derivedDropoffDate;
+
+@property (nonatomic, assign, readonly) BOOL isAtAirport;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CarTrawlerSDK.xcframework/ios-arm64_x86_64-simulator/CarTrawlerSDK.framework/Headers/CTNavigationDetails.h
@@ -0,0 +1,21 @@
+//
+//  CTNavigationDetails.h
+//  CarTrawlerSDK
+//
+//  Created by Rafael Ortiz on 02/09/2024.
+//  Copyright © 2024 Cartrawler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CTNavigationStackType.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTNavigationDetails : NSObject
+
+@property (nonatomic, readonly) CTNavigationStep step;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
