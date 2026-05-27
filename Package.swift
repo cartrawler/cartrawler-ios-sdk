410f0061d75045ec8fc1a4a1b52e6a620b8e3113
rsortiz <sh3llbr2@gmail.com>
2026-05-27 15:57:07 +0100

    Release 16.9.0

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CarTrawlerSDK",
-            url: "https://github.com/cartrawler/cartrawler-ios-sdk/releases/download/16.8.0/CarTrawlerSDK.xcframework.zip",
-            checksum: "c7d068ede843582a0bd643137e997379ab67027b5b91773c3d377787c2ca6107"
+            url: "https://github.com/cartrawler/cartrawler-ios-sdk/releases/download/16.9.0/CarTrawlerSDK.xcframework.zip",
+            checksum: "353eed97242f3d45f09fd599f180178e544d068282d208a2ee8e73dc9b5e0bdc"
         )
     ]
 )
